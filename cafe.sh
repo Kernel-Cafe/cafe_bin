a0c2435246b14affc91e5dfccc265082a2fb3dbe
Nizam Moidu <me@maxters.net>
2014-06-21 23:38:07 +0400

    check device config exists

--- cafe.sh
+++ cafe.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-# devices=()
-# export devices
-# devices_files=`find ./ -name "cafe_device.sh"`
-# for dv in $devices_files
-# do
-#   echo sourcing $dv
-#   source $dv
-# done
-# 
-# for dci in "${devices[@]}"; do
-#   echo $dci
-# done
-$KCLEAN=1
+
 source bin/setup.sh
 fpath=`echo $1 | sed  's/_/\//g'`
 fpath="device/$fpath/cafe_device.sh"
-source $fpath
-prepare
-print_config
-build_kernel
-make_bootimage
-make_zip
-make_dist
-print_finals
+if [[ -e $fpath ]]
+then
+    source $fpath
+    prepare
+    print_config
+    build_kernel
+    make_bootimage
+    make_zip
+    make_dist
+    print_finals
+else
+    echo "usage ./cafe.sh <device_name>"
+fi
